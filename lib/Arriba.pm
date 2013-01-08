3b43a0d9ac9682de3524ea712ed677cc616b9060
Michal Wojciechowski <odyniec@odyniec.net>
2013-01-08 19:35:30 +0100

    Add synopsis

--- lib/Arriba.pm
+++ lib/Arriba.pm
@@ -9,6 +9,22 @@ our $VERSION = '0.01';
 1;
 __END__
 
+=head1 SYNOPSIS
+
+Launch a plain HTTP server listening on port 5080:
+
+    arriba --listen :5080
+
+Launch an HTTPS server on port 5443, no SPDY:
+
+    arriba --listen :5443 --listen-ssl 5443 --ssl-cert-file cert.pem \
+        --ssl-key-file key.pem
+
+Launch an HTTPS server with SPDY support:
+
+    arriba --listen :5443 --listen-ssl 5443 --ssl-cert-file cert.pem \
+        --ssl-key-file key.pem --enable-spdy
+
 =head1 DESCRIPTION
 
 Description coming soonish.
